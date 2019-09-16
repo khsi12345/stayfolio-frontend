abe7cdba5f046255450a28b1966dd60dd6ad1add
khsi12345 <khsi6237@gmail.com>
2019-09-16 23:33:44 +0900

    Pick 페이지 레이아웃 구조 구성 진행 및 개발환경 추가 구축

--- src/Pages/Pick.js
+++ src/Pages/Pick.js
@@ -1,10 +1,74 @@
 import React , {memo}from 'react';
 import styled from 'styled-components'
+import theme from 'Components/Theme';
+import {device} from 'Components/Device';
 
+
+const PickWrap = styled.div`
+  margin-top: 102px;
+  margin-left: auto;
+  margin-right: auto;
+  min-height: 934px;
+  @media ${device.tablet} {
+    width: 750px;
+  }
+  @media ${device.desktop} {
+    width: 970px;
+  }
+  @media ${device.wide} {
+    width: 1170px;
+  }
+`
+
+const PickMainWrap = styled.div`
+`
+const PickMainHeader = styled.h2`
+  margin-top: 60px;
+  margin-bottom: 10px;
+  padding: 30px 0 40px;
+  border-top: 7px solid ${theme.DarkGrayBG};
+  height: 125px;
+  font-weight: 700;
+  font-size: 32px;
+`
+const PickMainHeaderSmall = styled.small`
+  display:block;
+  margin-top: 3px;
+  color: ${theme.FontGray};
+  font-weight: 400;
+  font-size: 14px;
+
+`
+const PickMainContainer = styled.div`
+`
+const PickMain = styled.div`
+  margin: 0 -15px;
+  &::before {
+    display: table;
+    content: " ";
+  }
+  &::after {
+    display: table;
+    content: " ";
+    clear: both;
+  }
+`
+// const PickPageNum = styled.div`
+// `
 const Pick = memo( () => {
     return(
       <>
-        
+        <PickWrap>
+          <PickMainWrap>
+            <PickMainHeader>
+              PICK
+              <PickMainHeaderSmall>매일 하루 한번! 스테이폴리오가 추천합니다!</PickMainHeaderSmall>
+            </PickMainHeader>
+            <PickMainContainer>
+              <PickMain />
+            </PickMainContainer>
+          </PickMainWrap>
+        </PickWrap>
       </>
     )
 });
